a666d90b234695fa7304b3f5a26acebe7b256c2d
cyberdreams8 <nethra.nayak23@spit.ac.in>
2025-08-18 14:50:41 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-
+// import express from "express"
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 // Load environment variables
 dotenv.config({
     path: './env'
 });
 
+// const app = express();
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at ${process.env.PORT}`);
+        
+    })
+
+
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed!!!", err);
+    
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (reuestHandler) => {
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = (fn) => {async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code||500).jso({
+//             success: false,
+//             message: error.message,
+//         })
+//     }
+// }}
